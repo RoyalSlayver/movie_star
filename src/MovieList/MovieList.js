ac6de354616b7514501e879a492b4296e584f326
RoyalSlayver <ivan_homex1@hotmail.com>
2018-10-03 17:52:47 +0200

    TextBox + Card

--- src/MovieList/MovieList.js
+++ src/MovieList/MovieList.js
@@ -3,30 +3,56 @@ import "./MovieList.css";
 import { Link } from "react-router-dom";
 import { Button, Icon, Form, TextArea } from "semantic-ui-react";
 import { Flex, Box } from "rebass";
+import { Card } from "primereact/card";
+import "primereact/resources/themes/nova-light/theme.css";
+import "primereact/resources/primereact.min.css";
 
-const SearchBar = changeItemValue => (
+const SearchBar = ({ changeItemValue, onCreateItem, ItemValue }) => (
   <Form>
     <TextArea
       autoHeight
-      placeholder="Movie Name"
+      placeholder={"Movie Name"}
+      value={ItemValue}
       rows={1}
-      onChange={e => changeItemValue(e.target.placeholder)}
+      style={{ width: "90%" }}
+      onChange={e => changeItemValue(e.target.value)}
     />
+    <Button icon onClick={onCreateItem}>
+      <Icon name="plus" />
+    </Button>
   </Form>
 );
 
 export default class MovieList extends React.Component {
   state = {
-    ItemValue: ""
+    ItemValue: "",
+    Item: ""
   };
 
-  changeItemValue(placeholder) {
+  changeItemValue(value) {
     this.setState({
-      ItemValue: placeholder
+      ItemValue: value
     });
   }
 
+  PushItem() {
+    this.setState({
+      Id: Date.now(),
+      Item: this.state.ItemValue
+    });
+  }
+  PrintOut() {
+    console.log(this.state.Item);
+  }
+
   render() {
+    const header = <img alt="Card" src="/public/clack.jpg" />;
+    const footer = (
+      <span>
+        <Button label="Favorite" icon="star" />
+      </span>
+    );
+
     return (
       <React.Fragment>
         <Flex>
@@ -41,12 +67,29 @@ export default class MovieList extends React.Component {
           <Box p={3} width={1 / 2} color="white" bg="black">
             <SearchBar
               ItemValue={this.state.ItemValue}
-              changeItemValue={placeholder => this.changeItemValue(placeholder)}
+              changeItemValue={value => this.changeItemValue(value)}
+              onCreateItem={() => this.PushItem()}
             />
           </Box>
         </Flex>
         <div className="landing-bg">
-          <div className="landing-content" />
+          <div className="landing-content">
+            <Card
+              title="Advanced Card"
+              subTitle="Subtitle"
+              style={{ width: "360px" }}
+              className="ui-card-shadow"
+              footer={footer}
+              header={header}
+            >
+              <div>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Inventore sed consequuntur error repudiandae numquam deserunt
+                quisquam repellat libero asperiores earum nam nobis, culpa
+                ratione quam perferendis esse, cupiditate neque quas!
+              </div>
+            </Card>
+          </div>
         </div>
       </React.Fragment>
     );
